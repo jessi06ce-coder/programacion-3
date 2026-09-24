6539f5fb1ae5861760b8a502d6fd5c965957d832
jessi06ce-coder <jessi06ce@gmail.com>
2026-09-24 14:51:55 -0300

    Update index.py

--- unidad-2-poo/index.py
+++ unidad-2-poo/index.py
@@ -1,1 +1,64 @@
+"""
+---------------------------------------------------------
+---------> FECHA DE ENTREGA: 9 DE OCTUBRE <--------------
+---------------------------------------------------------
 
+----| PARTE 1 |---
+
+ 1) ¿Qué es GitHub y para qué se utiliza?
+      Es una plataforma en la nube que permite subir tus proyectos. Vendría a ser como una red 
+      social para programadores, en donde publicas tus codigos, otros programadores pueden verlo y
+      modificarlo para agregarle cosas o mejorarlo. e inclsuo en mi caso me ha servido para 
+      volver mi pagina web en link de los cuales las persoans puede acceder directamente
+      a la pagina con tan solo que les envie el link
+      
+ 2) ¿Qué es un repositorio?
+      Es donde guardas todo el código y los archivos de tus proyectos.
+      
+ 3) ¿Qué diferencia hay engre Git y GitHub?
+       GitHub es una plataforma basada en la nube que guarda repostorios, mientras que Git lo que hace 
+       es detectar actualizaciones en el codigo.
+       
+ 4) ¿Qué es un commit y para qué sirve?
+      Es una instantanea inmutable que documenta que cambios se hicieron, quien los hizo y por qué.
+      Permitiendo revertir errores, facilitar la colaboracion, garantizar integridad,etc.
+      
+ 5) ¿Qué función cumple un archivo README.md?
+       El archivo README.md es la carta de presentación y documentación principal de un repositorio 
+       de código, diseñado para explicar qué es el proyecto, para qué sirve y cómo utilizarlo.
+       
+ 6) ¿Qué diferencia existe entre un repositorio público y uno privado?
+       Un repositorio publico puede verlo cualquier persona de internet, mientras que uno privado 
+       solo pueden verlo el propietario y la gente que él invite.
+       
+ 7) ¿Por qué puede ser útil GitHub para un estudiante de programación?
+      Puede ser bastante útil porque ya le deja un registro de seguimiento de actividad de programacion
+      a pesar de no haber trabajado para alguna empresa, sin mencionar que podria aprender como 
+      mejorarlo sin necesidad de hacer mucho ya que la gente al ver su repositorio puede modificarlo 
+      (ya sea para bien o mal). Ademas, si bien no es una cuenta de Linkedyn o un CV, suma, y genera 
+      trayectoria ya qeu no sería que arrancas desde 0, sino que ya tenes un espacio donde creas 
+      software funcional.
+
+----| Parte Tres |----
+
+12) ¿Qué fue lo mas sencillo y qué fue lo mas dificil de crear el repositorio?
+      Lo mas facil fue hacer la carpeta "unidad-2-poo", y lo mas dificil... tal vez encontrar para crear un archivo README.md
+      aunque no fue tan dificl en realidad ya que al crear el repositorio una de las opciones de abajo te dice si queres
+      que tenga un archivo README.md o no.
+
+13) ¿Qué ventaja encontras en guardar tus ejercicios en GitHub además de tenerlos en una carpeta de Drive?
+      - Es mucho mas comodo que traer a todas las clases el pendrive para bucar la carpera en especifico en donde hciimos 'x' 
+      proyecto, o incluso encontrarle errores y el uso es bastante práctico.
+      - A estar en un repositorio público, ya te genera un historial frente a als persoasn de internet de que vos hiciste esos
+      proyectos y, a pesar de que siempre eventualmente nos avergonzamos de nuestro primeros proyectos, a lo largo del tiempo
+      terminan volviendose donde todo comenzó, ya qeu si esperaramos a ahcer algo perfecto taradariamos ams en publicar, 
+      o incluso podriamos no publicarlo nunca (uno de lso pasos en donde la gente más se traba, junto con el de empezar. Yo tengo 
+      la teroia de que lo mas dificl es arrancar y exponer. aplicado a cualquier area, no solo la programacion)
+     - Es mas didactico el trabajo de clase, sin emncionar que nos ahce manejarnos pro tecnologias actuales, ya que Git y GitHub son 
+     sitios muy conocidos y utilizados actualmente
+
+14) ¿Qué concepto de GitHub todavía te genera dudas?
+      Ninguno realmente. solo em falta mandarme mas a usar lo que todavia no he probado (Issues, Insights, pull request,etc) como 
+      para saber que hace y si me sirve a mi para algo o no. Es cosa de meterle mas mano.
+
+"""
